fa65c404ece490d92857a9016e8cb1335d4976f9
Mr Rapxang <kunganyima171@gmail.com>
2024-11-13 13:16:28 -0500

    Assignment of statical computing "

--- Assignment 3/Assignemnt/Cyber threats/cyberbullying.py
+++ Assignment 3/Assignemnt/Cyber threats/cyberbullying.py
@@ -28,6 +28,109 @@ print("\nThe correlation between Cyber Bullying and Click on Unknown Link is",co
 
 
 
+import matplotlib.pyplot as plt
+import numpy as np
+y= df['Cyber Bullying']
+t_s= df['Time Spent']
+p_s = df['Password Security']
+e_s=df['Enable Security']
+s_l= df['Sharing Location']
+a_u=df['Accept Unknown']
+n_f=df['Number of Friends']
+n_g=df['Number of Groups']
+c_l=df['Click on Unknown Link']
+
+
+
+
+# plotting the data
+plt.suptitle('Correlation between Cyber Threat Cyber Bullying and independent variables')
+# adds the title
+plt.title('Correlation')
+plt.subplot(3,2,1)
+
+# plot the data
+plt.scatter(t_s, y)
+# fits the best fitting line to the data
+plt.plot(np.unique(t_s),
+		np.poly1d(np.polyfit(t_s, y, 1))
+		(np.unique(t_s)), color='red')
+# Labelling axes
+plt.xlabel('Time Spent')
+plt.ylabel('Cyber Bullying')
+
+
+
+plt.subplot(3,2,2)
+# plot the data
+plt.scatter(p_s, y)
+# fits the best fitting line to the data
+plt.plot(np.unique(p_s),
+		np.poly1d(np.polyfit(p_s, y, 1))
+		(np.unique(p_s)), color='red')
+# Labelling axes
+plt.xlabel('Password Security')
+plt.ylabel('Cyber Bullying')
+
+
+
+plt.subplot(3,2,3)
+# plot the data
+plt.scatter(e_s, y)
+# fits the best fitting line to the data
+plt.plot(np.unique(e_s),
+		np.poly1d(np.polyfit(e_s, y, 1))
+		(np.unique(e_s)), color='red')
+# Labelling axes
+plt.xlabel('Enable Security')
+plt.ylabel('Cyber Bullying')
+
+
+
+plt.subplot(3,2,4)
+# plot the data
+plt.scatter(s_l, y)
+# fits the best fitting line to the data
+plt.plot(np.unique(s_l),
+		np.poly1d(np.polyfit(s_l, y, 1))
+		(np.unique(s_l)), color='red')
+# Labelling axes
+plt.xlabel('Sharing Location')
+plt.ylabel('Cyber Bullying')
+
+
+plt.subplot(3,2,5)
+# plot the data
+plt.scatter(a_u, y)
+# fits the best fitting line to the data
+plt.plot(np.unique(a_u),
+		np.poly1d(np.polyfit(a_u, y, 1))
+		(np.unique(a_u)), color='red')
+# Labelling axes
+plt.xlabel('Accept Unknown')
+plt.ylabel('Cyber Bullying')
+
+
+
+
+plt.subplot(3,2,6)
+# plot the data
+plt.scatter(c_l, y)
+# fits the best fitting line to the data
+plt.plot(np.unique(c_l),
+		np.poly1d(np.polyfit(c_l, y, 1))
+		(np.unique(c_l)), color='red')
+# Labelling axes
+plt.xlabel('Click on Unknown Link')
+plt.ylabel('Cyber Bullying')
+
+
+
+
+
+plt.show()
+
+
 
 
 
