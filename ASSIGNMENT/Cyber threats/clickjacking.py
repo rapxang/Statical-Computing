41cc89de6cecff4a45936a2c0abbe491c6bb5e29
Mr Rapxang <kunganyima171@gmail.com>
2024-11-14 17:10:05 -0500

    updated assignment

--- ASSIGNMENT/Cyber threats/clickjacking.py
+++ ASSIGNMENT/Cyber threats/clickjacking.py
@@ -27,111 +27,7 @@ print("\nThe correlation between Clickjacking and Number of Groups is",correlati
 print("\nThe correlation between Clickjacking and Click on Unknown Link is",correlation_coul)
 
 
-#
-#
-# import matplotlib.pyplot as plt
-# import numpy as np
-# y= df['Clickjacking']
-# t_s= df['Time Spent']
-# p_s = df['Password Security']
-# e_s=df['Enable Security']
-# s_l= df['Sharing Location']
-# a_u=df['Accept Unknown']
-# n_f=df['Number of Friends']
-# n_g=df['Number of Groups']
-# c_l=df['Click on Unknown Link']
-#
-#
-#
-#
-# # plotting the data
-# plt.suptitle('Correlation between Cyber Threat Clickjacking and independent variables')
-# # adds the title
-# plt.title('Correlation')
-# plt.subplot(3,2,1)
-#
-# # plot the data
-# plt.scatter(t_s, y)
-# # fits the best fitting line to the data
-# plt.plot(np.unique(t_s),
-# 		np.poly1d(np.polyfit(t_s, y, 1))
-# 		(np.unique(t_s)), color='red')
-# # Labelling axes
-# plt.xlabel('Time Spent')
-# plt.ylabel('Clickjacking')
-#
-#
-#
-# plt.subplot(3,2,2)
-# # plot the data
-# plt.scatter(p_s, y)
-# # fits the best fitting line to the data
-# plt.plot(np.unique(p_s),
-# 		np.poly1d(np.polyfit(p_s, y, 1))
-# 		(np.unique(p_s)), color='red')
-# # Labelling axes
-# plt.xlabel('Password Security')
-# plt.ylabel('Clickjacking')
-#
-#
-#
-# plt.subplot(3,2,3)
-# # plot the data
-# plt.scatter(e_s, y)
-# # fits the best fitting line to the data
-# plt.plot(np.unique(e_s),
-# 		np.poly1d(np.polyfit(e_s, y, 1))
-# 		(np.unique(e_s)), color='red')
-# # Labelling axes
-# plt.xlabel('Enable Security')
-# plt.ylabel('Clickjacking')
-#
-#
-#
-# plt.subplot(3,2,4)
-# # plot the data
-# plt.scatter(s_l, y)
-# # fits the best fitting line to the data
-# plt.plot(np.unique(s_l),
-# 		np.poly1d(np.polyfit(s_l, y, 1))
-# 		(np.unique(s_l)), color='red')
-# # Labelling axes
-# plt.xlabel('Sharing Location')
-# plt.ylabel('Clickjacking')
-#
-#
-# plt.subplot(3,2,5)
-# # plot the data
-# plt.scatter(a_u, y)
-# # fits the best fitting line to the data
-# plt.plot(np.unique(a_u),
-# 		np.poly1d(np.polyfit(a_u, y, 1))
-# 		(np.unique(a_u)), color='red')
-# # Labelling axes
-# plt.xlabel('Accept Unknown')
-# plt.ylabel('Clickjacking')
-#
-#
-#
-#
-# plt.subplot(3,2,6)
-# # plot the data
-# plt.scatter(c_l, y)
-# # fits the best fitting line to the data
-# plt.plot(np.unique(c_l),
-# 		np.poly1d(np.polyfit(c_l, y, 1))
-# 		(np.unique(c_l)), color='red')
-# # Labelling axes
-# plt.xlabel('Click on Unknown Link')
-# plt.ylabel('Clickjacking')
-#
-#
-#
-#
-# plt.show()
-#
-#
-#
+
 import matplotlib.pyplot as plt
 
 # Plot Column1 vs Column2
@@ -154,7 +50,8 @@ plt.ylabel('Clickjacking')
 plt.title('Scatter plot of Correlation between Clickjacking and Behaviors')
 
 # Show legend
-plt.legend()
+plt.legend(loc = 'lower right')
 plt.grid(True)
+
 # Display the plot
 plt.show()
--- ASSIGNMENT/Cyber threats/cyberbullying.py
+++ ASSIGNMENT/Cyber threats/cyberbullying.py
@@ -27,110 +27,27 @@ print("\nThe correlation between Cyber Bullying and Number of Groups is",correla
 print("\nThe correlation between Cyber Bullying and Click on Unknown Link is",correlation_coul)
 
 
-
 import matplotlib.pyplot as plt
-import numpy as np
-y= df['Cyber Bullying']
-t_s= df['Time Spent']
-p_s = df['Password Security']
-e_s=df['Enable Security']
-s_l= df['Sharing Location']
-a_u=df['Accept Unknown']
-n_f=df['Number of Friends']
-n_g=df['Number of Groups']
-c_l=df['Click on Unknown Link']
+
+# Plot Column1 vs Column2
+plt.scatter(df['Time Spent'], df['Cyber Bullying'], label='Time Spent', color='blue')
+plt.scatter(df['Password Security'], df['Cyber Bullying'], label='Password Security', color='red')
+plt.scatter(df['Enable Security'], df['Cyber Bullying'], label='Enable Security', color='Green')
+plt.scatter(df['Sharing Location'], df['Cyber Bullying'], label='CSharing Location', color='Black')
+plt.scatter(df['Accept Unknown'], df['Cyber Bullying'], label='Accept Unknown', color='Pink')
+plt.scatter(df['Number of Friends'], df['Cyber Bullying'], label='Number of Friends', color='Orange')
+plt.scatter(df['Number of Groups'], df['Cyber Bullying'], label='Number of Groups', color='Purple')
+plt.scatter(df['Click on Unknown Link'], df['Cyber Bullying'], label='Click on Unknown Link', color='Brown')
 
 
 
-
-# plotting the data
-plt.suptitle('Correlation between Cyber Threat Cyber Bullying and independent variables')
-# adds the title
-plt.title('Correlation')
-plt.subplot(3,2,1)
-
-# plot the data
-plt.scatter(t_s, y)
-# fits the best fitting line to the data
-plt.plot(np.unique(t_s),
-		np.poly1d(np.polyfit(t_s, y, 1))
-		(np.unique(t_s)), color='red')
-# Labelling axes
-plt.xlabel('Time Spent')
+# Add labels and title
+plt.xlabel('Behaviors')
 plt.ylabel('Cyber Bullying')
+plt.title('Scatter plot of Correlation between Cyber Bullying and Behaviors')
+# Show legend
+plt.legend(loc = 'lower right')
+plt.grid(True)
 
-
-
-plt.subplot(3,2,2)
-# plot the data
-plt.scatter(p_s, y)
-# fits the best fitting line to the data
-plt.plot(np.unique(p_s),
-		np.poly1d(np.polyfit(p_s, y, 1))
-		(np.unique(p_s)), color='red')
-# Labelling axes
-plt.xlabel('Password Security')
-plt.ylabel('Cyber Bullying')
-
-
-
-plt.subplot(3,2,3)
-# plot the data
-plt.scatter(e_s, y)
-# fits the best fitting line to the data
-plt.plot(np.unique(e_s),
-		np.poly1d(np.polyfit(e_s, y, 1))
-		(np.unique(e_s)), color='red')
-# Labelling axes
-plt.xlabel('Enable Security')
-plt.ylabel('Cyber Bullying')
-
-
-
-plt.subplot(3,2,4)
-# plot the data
-plt.scatter(s_l, y)
-# fits the best fitting line to the data
-plt.plot(np.unique(s_l),
-		np.poly1d(np.polyfit(s_l, y, 1))
-		(np.unique(s_l)), color='red')
-# Labelling axes
-plt.xlabel('Sharing Location')
-plt.ylabel('Cyber Bullying')
-
-
-plt.subplot(3,2,5)
-# plot the data
-plt.scatter(a_u, y)
-# fits the best fitting line to the data
-plt.plot(np.unique(a_u),
-		np.poly1d(np.polyfit(a_u, y, 1))
-		(np.unique(a_u)), color='red')
-# Labelling axes
-plt.xlabel('Accept Unknown')
-plt.ylabel('Cyber Bullying')
-
-
-
-
-plt.subplot(3,2,6)
-# plot the data
-plt.scatter(c_l, y)
-# fits the best fitting line to the data
-plt.plot(np.unique(c_l),
-		np.poly1d(np.polyfit(c_l, y, 1))
-		(np.unique(c_l)), color='red')
-# Labelling axes
-plt.xlabel('Click on Unknown Link')
-plt.ylabel('Cyber Bullying')
-
-
-
-
-
+# Display the plot
 plt.show()
-
-
-
-
-
